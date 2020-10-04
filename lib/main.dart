f84d5831d5f268016f44b75ae103dc9764ed152d
sonawane-vivek <viveksonawane600@gmail.com>
2020-10-04 22:53:01 +0530

    Improved-overall-ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -29,56 +30,69 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Align(
-          alignment: Alignment.topCenter,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text(
-                  'Hacktoberfest 2019',
-                  style: TextStyle(fontSize: 30),
-                ),
-                Text(
-                  'Hacktoberfest 2019',
-                  style: TextStyle(fontSize: 10),
-                ),
-                Text(
-                  'Hacktoberfest 2019',
-                  style: TextStyle(fontSize: 12),
-                ),
-                Text(
-                  'Hacktoberfest 2019',
-                  style: TextStyle(fontSize: 20),
-                ),
-                Text(
-                  'Hacktoberfest 2019',
-                ),
-                Text(
-                  'Hacktoberfest 2019',
-                ),
-                Text(
-                  'Hacktoberfest 2019',
-                  style: TextStyle(fontSize: 24),
-                ),
-                Text(
-                  'Hacktoberfest 2019',
-                ),
-                Text(
-                  'Hacktoberfest 2019',
-                  style: TextStyle(fontSize: 26),
-                ),
-              ],
-            ),
-          ),
-        ),
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      appBar: AppBar(
+        title: Text(widget.title),
       ),
+      body: Column(
+        children: [
+          SizedBox(height: 50,),
+          Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
+                  ),
+                  color: Colors.white,
+                ),
+                child : Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text(
+                        'Hacktoberfest 2020',
+                        style: TextStyle(fontSize: 30),
+                      ),
+                      Text(
+                        'Hacktoberfest 2020',
+                        style: TextStyle(fontSize: 10),
+                      ),
+                      Text(
+                        'Hacktoberfest 2020',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                      Text(
+                        'Hacktoberfest 2020',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        'Hacktoberfest 2020',
+                      ),
+                      Text(
+                        'Hacktoberfest 2020',
+                      ),
+                      Text(
+                        'Hacktoberfest 2020',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                      Text(
+                        'Hacktoberfest 2020',
+                      ),
+                      Text(
+                        'Hacktoberfest 2020',
+                        style: TextStyle(fontSize: 26),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+          ),
+        ],
+      )
     );
   }
 }
